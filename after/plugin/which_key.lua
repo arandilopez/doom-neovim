410aa4eea46373f2052a6f8bd44d58769e4b4625
Arandi Lopez <arandilopez.93@gmail.com>
2021-11-07 00:52:22 -0600

    feat: configure plugins + whick_key bidnings + telescope finder

---
+++ after/plugin/which_key.lua
@@ -0,0 +1,23 @@
+local wk = require("which-key")
+local nops = {mode = 'n', silent = true, noremap = true}
+
+wk.register({
+  ["<leader>"] = {
+    ["<space>"] = { "<cmd>Telescope find_files<cr>", "Find Files" },
+
+    b = {
+      name = "+Buffers",
+      b = { "<cmd>Telescope buffers<cr>", "Browse buffers" },
+    },
+
+    s = {
+      name = "+Search",
+      p = { "<cmd>Telescope live_grep<cr>", "Search in project" }
+    },
+
+    g = {
+      name = "+Git",
+      g = { "<cmd>Gtabedit :<cr>", "Open Git manager" }
+    }
+  },
+}, nops)
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,8 +1,32 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function()
+  -- Core functionality
   use 'wbthomason/packer.nvim'
   use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+  use { 'nvim-telescope/telescope.nvim', requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}} }
+  use 'tpope/vim-surround'
+  use 'tpope/vim-commentary'
+  use 'tpope/vim-endwise'
+  use 'andymass/vim-matchup'
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = {'kyazdani42/nvim-web-devicons', opt = true}
+  }
+  -- Mappings
+  use "folke/which-key.nvim"
+
+  -- LSP
+  use 'neovim/nvim-lspconfig'
+  use 'kabouzeid/nvim-lspinstall'
+  use 'glepnir/lspsaga.nvim'
+
+  -- VCS plugins
+  use 'tpope/vim-fugitive'
+  use { 'lewis6991/gitsigns.nvim', -- git added/removed in sidebar + inline blame
+    requires = { 'nvim-lua/plenary.nvim' },
+  }
+
   -- Colorscheme
   use 'navarasu/onedark.nvim'
 end)
