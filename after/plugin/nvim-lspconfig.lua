3d5f925d23fb8b773e33976069c2d23ebed2fdff
Arandi Lopez <arandilopez.93@gmail.com>
2021-11-08 17:41:40 -0600

    chore: remove lsp lua settings

--- after/plugin/nvim-lspconfig.lua
+++ after/plugin/nvim-lspconfig.lua
@@ -33,7 +33,7 @@ end
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { 'tsserver', 'solargraph', 'sumneko_lua' }
+local servers = { 'tsserver', 'solargraph' }
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup {
     on_attach = on_attach,
