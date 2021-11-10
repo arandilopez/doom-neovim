0a7ddc2434137e7d222548e32b4b485c06df54e6
Arandi Lopez <arandilopez.93@gmail.com>
2021-11-09 19:15:18 -0600

    feat: support for lua lsp

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,3 +1,4 @@
+require('lsp.lua_ls')
 require('lsp.typescript')
 require('lsp.ruby')
 
---
+++ lua/lsp/lua_ls.lua
@@ -0,0 +1,26 @@
+local config = require('lsp.config')
+
+require('lspconfig').sumneko_lua.setup {
+  on_attach = config.on_attach,
+  capabilities = config.capabilities,
+  settings = {
+    Lua = {
+      runtime = {
+        version = 'LuaJIT',
+        path = vim.split(package.path, ';')
+      },
+
+      diagnostic = {
+        globals = {'vim'}
+      },
+
+      workspace = {
+        library = {
+          [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+          [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true
+        },
+        maxPreload = 10000
+      }
+    }
+  }
+}
