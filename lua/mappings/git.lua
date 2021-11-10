fced0ddbc8995b2277ae2b8b79cb38d7bd55f9b9
Arandi Lopez <arandilopez.93@gmail.com>
2021-11-10 14:04:29 -0600

    feat(git): toggle inline blame

--- lua/mappings/git.lua
+++ lua/mappings/git.lua
@@ -4,7 +4,8 @@ return {
       name = "+Git",
       g = { "<cmd>Gtabedit :<cr>", "Git status" },
       B = { "<cmd>Git blame<cr>", "Git blame" },
-      b = { "<cmd>Telescope git_branches<cr>", "Git branch switch" }
+      b = { "<cmd>Telescope git_branches<cr>", "Git branch switch" },
+      i = { "<cmd>Gitsigns toggle_current_line_blame<cr>", "Toogle inline git blame" },
     },
   }
 }
