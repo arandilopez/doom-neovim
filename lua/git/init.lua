1cdd210af4a0dcfe4c310808347fd58eee5b6610
Arandi Lopez <arandilopez.93@gmail.com>
2021-11-10 13:58:25 -0600

    feat(git): enable inline git blame

---
+++ lua/git/init.lua
@@ -0,0 +1,11 @@
+require('gitsigns').setup {
+  current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+  current_line_blame_opts = {
+    virt_text = true,
+    virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+    delay = 1000,
+  },
+  current_line_blame_formatter_opts = {
+    relative_time = true
+  },
+}
