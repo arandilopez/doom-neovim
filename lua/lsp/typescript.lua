ca709ae1e496a4456739295088f6d5d6fddbd91c
Arandi Lopez <arandilopez.93@gmail.com>
2021-11-12 17:36:18 -0600

    fix: lint

--- lua/lsp/typescript.lua
+++ lua/lsp/typescript.lua
@@ -3,9 +3,9 @@ local config = require('lsp.config')
 require('lspconfig').tsserver.setup {
   on_attach = config.on_attach,
   capabilities = config.capabilities,
-  filetypes = { 
-    "typescript", 
-    "typescriptreact", 
+  filetypes = {
+    "typescript",
+    "typescriptreact",
     "typescript.tsx",
     "javascript",
     "javascriptreact",
