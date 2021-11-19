6079e2cc05585194a64430bf6f5a599422bfa746
Arandi Lopez <arandilopez.93@gmail.com>
2021-11-19 13:03:09 -0600

    chore: improve git commands docs

--- lua/mappings/git.lua
+++ lua/mappings/git.lua
@@ -2,9 +2,9 @@ return {
   ["<Leader>"] = {
     g = {
       name = "+Git",
-      g = { "<cmd>Gtabedit :<cr>", "Git status" },
-      B = { "<cmd>Git blame<cr>", "Git blame" },
-      b = { "<cmd>Telescope git_branches<cr>", "Git branch switch" },
+      g = { "<cmd>Gtabedit :<cr>", "Open git status summary" },
+      B = { "<cmd>Git blame<cr>", "Git blame buffer" },
+      b = { "<cmd>Telescope git_branches<cr>", "Switch to a git branch" },
       i = { "<cmd>Gitsigns toggle_current_line_blame<cr>", "Toogle inline git blame" },
       s = { "<cmd>Telescope git_status<cr>", "Files in git status" },
     },
