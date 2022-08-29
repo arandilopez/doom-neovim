60023061bad77055c6a15fa08fb09956a43a48b8
Arandi Lopez <arandilopez.93@gmail.com>
2022-08-29 15:55:02 -0500

    feat: default languages

--- lua/tree-sitter/init.lua
+++ lua/tree-sitter/init.lua
@@ -1,4 +1,17 @@
 require("nvim-treesitter.configs").setup({
+  ensure_installed = {
+    "c",
+    "lua",
+    "rust",
+    "ruby",
+    "tsx",
+    "typescript",
+    "javascript",
+    "go",
+    "graphql",
+    "elixir",
+    "python",
+  },
   highlight = {
     enable = true,
   },
