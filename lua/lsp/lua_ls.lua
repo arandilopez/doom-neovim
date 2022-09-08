f81f7f1f2e942660b0917a7eef6f162877c7eea2
Arandi Lopez <arandilopez.93@gmail.com>
2022-09-08 17:25:02 -0500

    fix: use default lsp cmd

--- lua/lsp/lua_ls.lua
+++ lua/lsp/lua_ls.lua
@@ -1,29 +1,26 @@
-local sumneko_root_path = DATA_PATH .. "/lspinstall/lua"
-local sumneko_binary = sumneko_root_path .. "/sumneko-lua-language-server"
-local config = require('lsp.config')
+local config = require("lsp.config")
 
-require('lspconfig').sumneko_lua.setup {
-  cmd = { sumneko_binary, "-E", sumneko_root_path .. "/main.lua"},
+require("lspconfig").sumneko_lua.setup({
   on_attach = config.on_attach,
   capabilities = config.capabilities,
   settings = {
     Lua = {
       runtime = {
-        version = 'LuaJIT',
-        path = vim.split(package.path, ';')
+        version = "LuaJIT",
+        path = vim.split(package.path, ";"),
       },
 
       diagnostics = {
-        globals = {'vim', 'use'}
+        globals = { "vim", "use" },
       },
 
       workspace = {
         library = {
-          [vim.fn.expand('$VIMRUNTIME/lua')] = true,
-          [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true
+          [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+          [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
         },
-        maxPreload = 10000
-      }
-    }
-  }
-}
+        maxPreload = 10000,
+      },
+    },
+  },
+})
