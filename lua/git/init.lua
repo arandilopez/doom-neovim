696d8e3f12bd28cdb24c33fa89f771700134e0d2
Arandi Lopez <arandilopez.93@gmail.com>
2022-09-22 12:45:50 -0500

    fix: display git blame comments with dracula theme

--- lua/git/init.lua
+++ lua/git/init.lua
@@ -1,11 +1,14 @@
-require('gitsigns').setup {
+require("gitsigns").setup({
   current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
   current_line_blame_opts = {
     virt_text = true,
-    virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+    virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
     delay = 1000,
   },
   current_line_blame_formatter_opts = {
-    relative_time = true
+    relative_time = true,
   },
-}
+})
+
+-- Fixes git blame comments with Dracula theme
+vim.cmd([[highlight link GitSignsCurrentLineBlame Visual]])
