952392b0c12222a7f98f73b356f9318b57e28276
Arandi Lopez <arandilopez.93@gmail.com>
2022-12-02 18:52:12 -0600

    feat: refine and remove unused configuration for debugging

--- lua/debugging/init.lua
+++ lua/debugging/init.lua
@@ -1,5 +1,5 @@
-local dap, dapui = require("dap"), require("dapui")
-require("nvim-dap-virtual-text").setup()
+local dap = require("dap")
+local dapui = require("dapui")
 
 dapui.setup()
 
@@ -13,15 +13,13 @@ dap.listeners.before.event_exited["dapui_config"] = function()
   dapui.close()
 end
 
+require("nvim-dap-virtual-text").setup()
+
+-- Language support for dap
+
 require("dap-ruby").setup()
 require("dap-vscode-js").setup({
-  -- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
-  -- debugger_path = "(runtimedir)/site/pack/packer/opt/vscode-js-debug", -- Path to vscode-js-debug installation.
-  -- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
-  adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" }, -- which adapters to register in nvim-dap
-  -- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
-  -- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
-  -- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
+  adapters = { "pwa-node" },
 })
 
 for _, language in ipairs({ "typescript", "javascript" }) do
@@ -29,7 +27,7 @@ for _, language in ipairs({ "typescript", "javascript" }) do
     {
       type = "pwa-node",
       request = "attach",
-      name = "Attach",
+      name = "Attach debug session",
       processId = require("dap.utils").pick_process,
       cwd = "${workspaceFolder}",
     },
@@ -43,8 +41,8 @@ for _, language in ipairs({ "typescript", "javascript" }) do
     {
       type = "pwa-node",
       request = "launch",
-      name = "Debug Jest Tests",
-      -- trace = true, -- include debugger info
+      name = "Debug Jest Test",
+      trace = true, -- include debugger info
       runtimeExecutable = "node",
       runtimeArgs = {
         "./node_modules/jest/bin/jest.js",
@@ -57,6 +55,8 @@ for _, language in ipairs({ "typescript", "javascript" }) do
     },
   }
 end
+
+-- Debugging icons
 
 vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "DiagnosticSignError", linehl = "", numhl = "" })
 vim.fn.sign_define("DapBreakpointCondition", { text = "", texthl = "DiagnosticSignError", linehl = "", numhl = "" })
--- lua/mappings/debugger.lua
+++ lua/mappings/debugger.lua
@@ -9,18 +9,14 @@ return {
     d = {
       name = "+Debugger",
       b = { "<cmd>lua require('dap').toggle_breakpoint()<cr>", "Toggle breakpoint" },
-      c = { "<cmd>lua require('dap').continue()<cr>", "Continue to next breakpoint" },
-      u = { "<cmd>lua require('dap').step_over()<cr>", "Step over" },
-      i = { "<cmd>lua require('dap').step_into()<cr>", "Step into" },
-      o = { "<cmd>lua require('dap').step_out()<cr>", "Step out" },
       r = { "<cmd>lua require('dap').repl.open()<cr>", "Open REPL session" },
-      K = { "<cmd>lua require('dap.ui.widgets').hover()<cr>", "View the value of the expression under cursor" },
-      O = { "<cmd>lua require('dapui').toggle()<cr>", "Toggle DAP UI" },
-      C = {
+      o = { "<cmd>lua require('dapui').toggle()<cr>", "Toggle DAP UI" },
+      c = {
         "<Cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>",
         "Create a conditional breakpoint",
       },
       D = { "<Cmd>lua require'dap'.clear_breakpoints()<CR>", "Clear all breakpoints" },
+      K = { "<cmd>lua require('dap.ui.widgets').hover()<cr>", "View the value of the expression under cursor" },
     },
   },
 }
