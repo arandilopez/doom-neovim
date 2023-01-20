cb84bcee0b9f780d0bb5dea18ec8060761bf634a
Arandi Lopez <arandilopez.93@gmail.com>
2023-01-20 12:41:38 -0600

    feat: add pomodoro plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -90,6 +90,18 @@ local setup = function()
     cmd = "MarkdownPreview",
     ft = { "markdown" },
   })
+  use({
+    "wthollingsworth/pomodoro.nvim",
+    requires = "MunifTanjim/nui.nvim",
+    config = function()
+      require("pomodoro").setup({
+        time_work = 25,
+        time_break_short = 5,
+        time_break_long = 20,
+        timers_to_long_break = 4,
+      })
+    end,
+  })
 end
 
 local options = {
