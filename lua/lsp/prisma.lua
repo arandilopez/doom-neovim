2bb8a87fe1a2f24c42bf169cc0b1564c5496e176
Arandi Lopez <arandilopez.93@gmail.com>
2023-04-25 19:41:06 -0600

    chore: remove commented code

--- lua/lsp/prisma.lua
+++ lua/lsp/prisma.lua
@@ -3,11 +3,6 @@ local config = require("lsp.config")
 require("lspconfig").prismals.setup({
   cmd = { "prisma-language-server", "--stdio" },
   filetypes = { "prisma" },
-  -- settings = {
-  --   prisma = {
-  --     prismaFmtBinPath = "pfmt",
-  --   },
-  -- },
   on_attach = function(client, buffer)
     config.on_attach(client, buffer)
     -- Handle formatting for prisma files with LSP
