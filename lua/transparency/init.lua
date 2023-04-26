bfe990aad3e2019dcc0d7c3d216c42161ecc24e8
Arandi Lopez <arandilopez.93@gmail.com>
2023-04-26 12:09:53 -0600

    feat: remove commented and deprecated options

--- lua/transparency/init.lua
+++ lua/transparency/init.lua
@@ -1,5 +1,4 @@
 require("transparent").setup({
-  enable = false, -- boolean: enable transparent
   extra_groups = { -- table/string: additional groups that should be cleared
     -- In particular, when you set it to 'all', that means all available groups
 
@@ -11,5 +10,4 @@ require("transparent").setup({
     "BufferLineSeparator",
     "BufferLineIndicatorSelected",
   },
-  exclude = {}, -- table: groups you don't want to clear
 })
