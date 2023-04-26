128243d643fd1376566464958d582d7356e5b329
Arandi Lopez <arandilopez.93@gmail.com>
2023-04-25 20:45:33 -0600

    feat: add support for sorbet

--- lua/lsp/ruby.lua
+++ lua/lsp/ruby.lua
@@ -1,19 +1,30 @@
-local config = require('lsp.config')
+local config = require("lsp.config")
+local lsp = require("lspconfig")
+local utils = require("lspconfig/util")
 
-require('lspconfig').solargraph.setup {
+lsp.solargraph.setup({
   cmd = { "solargraph", "stdio" },
   filetypes = { "ruby" },
   init_options = {
-    formatting = true
+    formatting = true,
   },
   settings = {
     solargraph = {
-      diagnostics = true
-    }
+      diagnostics = true,
+    },
   },
   flags = {
-    debounce_text_changes = 150
+    debounce_text_changes = 150,
   },
   on_attach = config.on_attach,
   capabilities = config.capabilities,
-}
+  root_dir = utils.root_pattern(".rubocop.yml"),
+})
+
+lsp.sorbet.setup({
+  cmd = { "srb", "tc", "--lsp" },
+  filetypes = { "ruby" },
+  on_attach = config.on_attach,
+  capabilities = config.capabilities,
+  root_dir = utils.root_pattern("sorbet/config"),
+})
