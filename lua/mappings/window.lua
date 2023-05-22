5e5bb28ee290ec91dbc220f4bf8369a3f6183717
Arandi Lopez <arandilopez.93@gmail.com>
2023-05-22 11:51:00 -0600

    feat: split bindings

--- lua/mappings/window.lua
+++ lua/mappings/window.lua
@@ -2,10 +2,20 @@ return {
   ["<Leader>"] = {
     w = {
       name = "+Window",
-      h = {"<C-W><C-H>", "Move focus to left window"},
-      j = {"<C-W><C-J>", "Move focus to above window"},
-      k = {"<C-W><C-K>", "Move focus to below window"},
-      l = {"<C-W><C-L>", "Move focus to right window"},
-    }
-  }
+      h = { "<C-W><C-H>", "Move focus to left window" },
+      j = { "<C-W><C-J>", "Move focus to above window" },
+      k = { "<C-W><C-K>", "Move focus to below window" },
+      l = { "<C-W><C-L>", "Move focus to right window" },
+    },
+  },
+  s = {
+    name = "+Split",
+    s = { "<C-W>s", "Split window horizontally" },
+    v = { "<C-W>v", "Split window vertically" },
+
+    h = { "<C-W>H", "Move window to far left" },
+    j = { "<C-W>J", "Move window to far bottom" },
+    k = { "<C-W>K", "Move window to far top" },
+    l = { "<C-W>L", "Move window to far right" },
+  },
 }
