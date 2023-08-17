499a8dfb1d4a4e1fd0eb7db90492dd0989f2f212
Arandi Lopez <arandilopez.93@gmail.com>
2023-08-17 17:42:46 -0600

    feat: show relative path + filename in status line

--- lua/langs/init.lua
+++ lua/langs/init.lua
@@ -1,9 +1,22 @@
 vim.filetype.add({
+  -- Detect and assign filetype based on the extension of the filename
   extension = {
     mdx = "mdx",
+    -- log = "log",
+    -- conf = "conf",
+    -- env = "dotenv",
   },
-  filename = {},
-  pattern = {},
+  -- Detect and apply filetypes based on the entire filename
+  filename = {
+    -- [".env"] = "dotenv",
+    -- ["tsconfig.json"] = "jsonc",
+  },
+  -- Detect and apply filetypes based on certain patterns of the filenames
+  pattern = {
+    -- INFO: Match filenames like - ".env.example", ".env.local" and so on
+    -- ["%.env%.[%w_.-]+"] = "dotenv",
+    -- ["tsconfig.[%w_.-]+.json"] = "jsonc",
+  },
 })
 
 vim.treesitter.language.register("markdown", "mdx")
