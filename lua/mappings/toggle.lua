3e9aa2b6df40abc82612c98f2b258870274cc79a
Arandi Lopez <arandilopez.93@gmail.com>
2023-10-03 10:20:41 -0600

    feat: add term floating window

--- lua/mappings/toggle.lua
+++ lua/mappings/toggle.lua
@@ -4,6 +4,7 @@ return {
       name = "+Toggle",
       c = { "<cmd>Telescope colorscheme<cr>", "Colorscheme" },
       n = { "<cmd>set rnu!<cr>", "Relative numbering" },
+      t = { "<cmd>Lspsaga term_toggle<cr>", "Terminal" },
       z = { "<cmd>Goyo<cr>", "Zen mode" },
     },
   },
