2577b72c8c1e3201096ff5edc6fb97fbfe163f62
Arandi Lopez <arandilopez.93@gmail.com>
2024-02-27 10:41:20 -0600

    feat(lsp): disable solargraph in favor of ruby-lsp

--- lua/lsp/ruby.lua
+++ lua/lsp/ruby.lua
@@ -2,39 +2,14 @@ local config = require("lsp.config")
 local lsp = require("lspconfig")
 local utils = require("lspconfig/util")
 
-local function ruby_root_pattern(...)
-  local patterns = { ".git", "Gemfile" }
-  return utils.root_pattern(unpack(patterns))(...)
-end
-
 local function sorbet_root_pattern(...)
   local patterns = { "sorbet/config" }
   return utils.root_pattern(unpack(patterns))(...)
 end
 
-lsp.solargraph.setup({
-  cmd = { "solargraph", "stdio" },
-  filetypes = { "ruby" },
-  init_options = {
-    formatting = true,
-  },
-  settings = {
-    solargraph = {
-      diagnostics = true,
-    },
-  },
-  flags = {
-    debounce_text_changes = 150,
-  },
+lsp.ruby_ls.setup({
   on_attach = config.on_attach,
   capabilities = config.capabilities,
-  root_dir = function(fname)
-    local is_sorbet = sorbet_root_pattern(fname)
-    if is_sorbet then
-      return false
-    end
-    return ruby_root_pattern(fname)
-  end,
 })
 
 lsp.sorbet.setup({
